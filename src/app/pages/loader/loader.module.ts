d770e8dc5b433053515b2b4936a7ef6b0ad483d9
inie-mini <79155995+inie-mini@users.noreply.github.com>
2022-03-08 16:55:18 +0100

    Added Login screen

--- src/app/pages/loader/loader.module.ts
+++ src/app/pages/loader/loader.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LoaderPageRoutingModule } from './loader-routing.module';
-
 import { LoaderPage } from './loader.page';
+// import { FolderPage } from 'src/app/folder/folder.page';
 
 @NgModule({
   imports: [
@@ -17,4 +15,4 @@ import { LoaderPage } from './loader.page';
   ],
   declarations: [LoaderPage]
 })
-export class LoaderPageModule {}
+export class LoaderPageModule { }
